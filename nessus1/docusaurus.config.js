923d2f5f8db3b3fe147d4d4251402fa1fd9c22e1
Ramiro Torres <ramixalex@hotmail.com>
2022-11-02 19:31:46 +0100

    Update docusaurus.config.js

--- nessus1/docusaurus.config.js
+++ nessus1/docusaurus.config.js
@@ -32,6 +32,10 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
+        docs: {
+          routeBasePath: '/docs/', // Serve the docs at the site's root
+          /* other docs plugin options */
+        },
         // docs: {
         //   sidebarPath: require.resolve('./sidebars.js'),
         //   // Please change this to your repo.
